2fd7bf3309fa08436eaac3f4357d5a70f87f24c3
Imamov Erzhigit <erjigit17@gmail.com>
2022-06-08 10:45:32 +0600

    feat: add processor.js

---
+++ src/processor.js
@@ -0,0 +1,30 @@
+import {
+  exit,
+  ls,
+  cd,
+  up,
+  cat,
+  add,
+  rn,
+  cp,
+  mv,
+  rm,
+} from './helpers/index.js'
+
+export const commands = new Map()
+commands.set('.e',    {function: exit, })
+commands.set('.exit', {function: exit, })
+commands.set('up',    {function: up,   })
+commands.set('cd',    {function: cd,   requireArgs: ['pathToDirectory']})
+commands.set('ls',    {function: ls,   })
+commands.set('cat',   {function: cat,  requireArgs: ['pathToFile']})
+commands.set('add',   {function: add,  requireArgs: ['pathToNewFile']})
+commands.set('rn',    {function: rn,   requireArgs: ['pathToFile', 'pathToNewFile']})
+commands.set('cp',    {function: cp,   requireArgs: ['pathToFile', 'pathToDirectory']})
+commands.set('mv',    {function: mv,   requireArgs: ['pathToFile', 'pathToDirectory']})
+commands.set('rm',    {function: rm,   requireArgs: ['pathToFile']})
+
+
+export async function processor(command, context, arg1, arg2) {
+  await commands.get(command).function(context, arg1, arg2)
+}
